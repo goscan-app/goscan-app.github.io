a905f939ac0380d879892b4ee330eb92bb960f12
Chandan Gowda <chandangowda@Chandans-MBP.lan>
2024-10-08 21:55:17 -0500

    update: base vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[goscan-app.github.io]/"
+  base: "/[goscan-app]/"
 })
