62d0a267269c0298bd6ace60360308e732bac458
Chandan Gowda <chandangowda@Chandans-MBP.lan>
2024-10-12 18:40:13 -0500

    update: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import './App.css'
+
 function App() {
   return (
     <>
+       <div className="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+      </div>
+
       <h1>Welcome to GoScan Home Page!</h1>
-
-
       <h6>Copyrights of Pratheek</h6>
     </>
   )
